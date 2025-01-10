64255c3c0ced67dd6687e6e0ffda01cbed6f9d0a
dhruva-vidhant <66923429+dhruva-vidhant@users.noreply.github.com>
2025-01-10 21:53:36 +0000

    Did first problem

--- Main.java
+++ Main.java
@@ -1,1 +1,8 @@
-
+import java.util.stream.IntStream;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("#1:" + IntStream.range(0,1000)
+            .filter(x -> x % 3 == 0 || x % 5 == 0).sum());
+        
+    }
+}
